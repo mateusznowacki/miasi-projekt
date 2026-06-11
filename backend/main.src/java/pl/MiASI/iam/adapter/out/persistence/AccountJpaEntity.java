d5bff80ce1d65a0c537cdaf3ee41e6603d7387fc
mateusz-nowacki <mattynovy@outlook.com>
2026-06-11 21:10:17 +0200

    fix/ add column definition

--- backend/main.src/java/pl/MiASI/iam/adapter/out/persistence/AccountJpaEntity.java
+++ backend/main.src/java/pl/MiASI/iam/adapter/out/persistence/AccountJpaEntity.java
@@ -1,17 +1,27 @@
 package pl.MiASI.iam.adapter.out.persistence;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.MiASI.iam.domain.model.Role;
+
 import java.util.UUID;
+
 @Entity
 @Table(name = "accounts")
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class AccountJpaEntity {
-    @Id private UUID id;
-    @Column(unique = true, nullable = false) private String email;
-    @Column(nullable = false) private String passwordHash;
-    @Enumerated(EnumType.STRING) private Role role;
-    @Column(nullable = false) private boolean active = false;
+    @Id
+    private UUID id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String passwordHash;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean active = false;
 }
